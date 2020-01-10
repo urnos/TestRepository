265b50583086819f19cdf9959aef5b8afdb98d87
Vanessa Urnos <urnos@shernet.sheridancollege.ca>
2020-01-10 13:46:28 -0500

    Initial Unity Commit

---
+++ TestProject/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public string name = "Vanessa";
+    public int health = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(name);
+    }
+}
